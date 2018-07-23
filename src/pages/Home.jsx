fa57e914d7617d96c2a952767407d3114692ff78
necokeine <necokeine@gmail.com>
2018-07-23 23:33:56 +0800

    Support new contract and support buy and sell.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,20 @@
 import React from "react";
 import { Button, Input, Select } from "antd";
 import intl from "react-intl-universal";
-import Nasa from "nasa.js";
+import nasa from "nasa.js";
 
 const backgroundImg = 'https://i.loli.net/2018/07/16/5b4c4a832a920.jpg'
-const Option = Select.Option;
+const Nasa = window.Nasa;
 
 const bannerStyle = {
     padding: `6rem`,
     color: `#fafafa`,
     width: "100%", minHeight: "48rem",
     background: `url(${backgroundImg})`, backgroundSize: 'cover'
+}
+
+const buttonStyle = {
+    margin: "0.5rem"
 }
 
 // const titleStyle = {
@@ -25,11 +29,12 @@ const bannerStyle = {
 
 
 var user_addr;
-var contract = 'n1zofV7tjSZgNqoDMLQNXuLLqdQmM2REAcL';
+var contract = 'n1rNoRtSYoR3mC2ATC4nXxZaNLYNtCon67o';
 
-function BuyEvent(e) {
+function ClaimEvent(e) {
     var args = []
-    window.Nasa.call(contract, "buy", args)
+    var option = {}
+    window.Nasa.call(contract, "claim", args, option)
     .then((payId) => {
         setTimeout(() => {
         }, 5000)
@@ -43,60 +48,165 @@ function BuyEvent(e) {
     })
 }
 
-function SellEvent(e) {
-    alert("Sell");
-}
-
-function ClaimEvent(e) {
-    alert("Claim")
-}
-
 
 export function initializeUserInfo() {
     window.Nasa.user.getAddr()
-    .then((addr) => {
-        user_addr = addr;
-        //alert(addr)
-    })
-    .catch((e) => {
-        //alert('Error: ' + e)
-    })
+        .then((addr) => {
+            user_addr = addr;
+            //alert(addr)
+        })
+        .catch((e) => {
+            alert('Error: ' + e)
+        })
 
     window.Nasa.contract.set({
         default: {
-            local:   'n1zofV7tjSZgNqoDMLQNXuLLqdQmM2REAcL',
-            testnet: 'n1zofV7tjSZgNqoDMLQNXuLLqdQmM2REAcL',
-            mainnet: 'n1zofV7tjSZgNqoDMLQNXuLLqdQmM2REAcL',
-        },
-        contract_article: {
-            /* ... */
-        },
-        contract_comment: {
-            /* ... */
+            local: 'n1rNoRtSYoR3mC2ATC4nXxZaNLYNtCon67o',
+            testnet: 'n1rNoRtSYoR3mC2ATC4nXxZaNLYNtCon67o',
+            mainnet: 'n1rNoRtSYoR3mC2ATC4nXxZaNLYNtCon67o',
         }
     })
 }
 
-export default function Home() {
-    initializeUserInfo();
-    const selectBefore = (
-        <Select defaultValue="{intl.get('homepage.buy')}" style={{ width: 90 }}>
-            <Option value="{intl.get('homepage.buy')}">{intl.get('homepage.buy')}</Option>
-            <Option value="{intl.get('homepage.sell)}">{intl.get('homepage.sell')}</Option>
-        </Select>
-    )
-    return (
-    <div className="index-page" style={{ marginTop: "-64px" }}>
-        <div className="banner" style={bannerStyle}>
-            <div> Current balance </div>
-            <div> {intl.get('homepage.buy')}: </div>
-            <Input id="buy_amount" placeholder="{homepage.buy_amount}" />
-            <Input addonBefore={ selectBefore } placeholder="Amount" />
-            <Button type="primary" size="large" onClick={BuyEvent}> {intl.get('homepage.buy_button')}</Button>
-            <Button type="primary" size="large" onClick={SellEvent}> {intl.get('homepage.sell_button')}</Button>
-            <Button type="primary" size="large" onClick={ClaimEvent}> {intl.get('homepage.claim_button')}</Button>
-                
-            <p> </p>
-        </div>
-    </div>)
+class BuyPopup extends React.Component {
+    BuyEvent(e) {
+        var args = []
+        var option = {
+            "value": document.getElementById("buy_amount").value
+        }
+        window.Nasa.call(contract, "buy", args, option)
+            .then((payId) => {
+                setTimeout(() => {
+                }, 5000)
+            })
+            .catch((e) => {
+                let msg = e.message
+                if (msg === window.Nasa.error.TX_REJECTED_BY_USER) {
+                    msg = '您已取消交易！'
+                }
+                alert(msg)
+            })
+    }
+
+    render() {
+        return (
+            <div className='popup'>
+                <div className='popup_inner'>
+                    <h1>{this.props.text}</h1>
+                    <p> </p>
+                    <Input
+                        {...this.props}
+                        id="buy_amount"
+                        placeholder="Input a amount in NAS"
+                        maxLength="25"
+                    />
+                    <Button type="primary" size="large" style={buttonStyle} onClick={this.BuyEvent}>
+                        {intl.get('homepage.buy_button')}
+                    </Button>
+                    <Button type="primary" size="large" style={buttonStyle} onClick={this.props.closePopup}>
+                        {intl.get('homepage.cancel_popup')}
+                    </Button>
+                </div>
+            </div >
+        );
+    }
 }
+
+class SellPopup extends React.Component {
+
+    SellEvent(e) {
+        var args = [document.getElementById("sell_amount").value]
+        var option = {}
+
+        window.Nasa.call(contract, "sell", args, option)
+            .then((payId) => {
+                setTimeout(() => {
+                }, 5000)
+            })
+            .catch((e) => {
+                let msg = e.message
+                if (msg === window.Nasa.error.TX_REJECTED_BY_USER) {
+                    msg = '您已取消交易！'
+                }
+                alert(msg)
+            })
+    }
+
+    render() {
+        return (
+            <div className='popup'>
+                <div className='popup_inner'>
+                    <h1>{this.props.text}</h1>
+                    <p> </p>
+                    <Input
+                        {...this.props}
+                        id="sell_amount"
+                        placeholder="Input a amount in Token"
+                        maxLength="25"
+                    />
+                    <Button type="primary" size="large" style={buttonStyle} onClick={this.SellEvent}>
+                        {intl.get('homepage.sell_button')}
+                    </Button>
+                    <Button type="primary" size="large" style={buttonStyle} onClick={this.props.closePopup}>
+                        {intl.get('homepage.cancel_popup')}
+                    </Button>
+                </div>
+            </div>
+        );
+    }
+}
+
+class Home extends React.Component {
+    constructor() {
+        super();
+        initializeUserInfo();
+        this.state = {
+            showPopup: false
+        };
+    }
+    toggleBuyPopup() {
+        this.setState({
+            showBuyPopup: !this.state.showBuyPopup
+        });
+    }
+    toggleSellPopup() {
+        this.setState({
+            showSellPopup: !this.state.showSellPopup
+        });
+    }
+    render() {
+        return (
+            <div className="index-page" style={{ marginTop: "-64px" }}>
+                <div className="banner" style={bannerStyle}>
+                    <div> Current balance </div>
+
+                    <Button type="primary" size="large" style={buttonStyle} onClick={this.toggleBuyPopup.bind(this)}>
+                        {intl.get('homepage.buy_button')}
+                    </Button>
+                    <Button type="primary" size="large" style={buttonStyle} onClick={this.toggleSellPopup.bind(this)}>
+                        {intl.get('homepage.sell_button')}
+                    </Button>
+                    <Button type="primary" size="large" style={buttonStyle} onClick={ClaimEvent}>
+                        {intl.get('homepage.claim_button')}
+                    </Button>
+                    {this.state.showBuyPopup ?
+                        <BuyPopup
+                            text="intl.get('homepage.buy_title')"
+                            closePopup={this.toggleBuyPopup.bind(this)}
+                        />
+                        : null
+                    }
+                    {this.state.showSellPopup ?
+                        <SellPopup
+                            text="intl.get('homepage.sell_title')"
+                            closePopup={this.toggleSellPopup.bind(this)}
+                        />
+                        : null
+                    }
+                </div>
+            </div>
+        );
+    }
+};
+
+export default Home;
