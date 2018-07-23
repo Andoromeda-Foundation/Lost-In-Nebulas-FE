43f57f8734fa1641f991586de8b76a90af6f2012
Frank Wei <frank@frankwei.xyz>
2018-07-23 14:15:37 +0800

    feat: 3 btns in Home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,8 @@
 import React from "react"
 import intl from "react-intl-universal";
+import { Button } from "antd";
+import "./style.css"
 // import config from "../config";
-import Brand from "../Brand-Outline.svg";
 
 const backgroundImg = 'https://i.loli.net/2018/07/16/5b4c4a832a920.jpg'
 
@@ -12,22 +13,13 @@ const bannerStyle = {
     background: `url(${backgroundImg})`, backgroundSize: 'cover'
 }
 
-// const titleStyle = {
-//     opacity: 1, transform: `translate(0px, 0px)`,
-//     fontSize: `68px`,
-//     color: `#fafafa`,
-//     fontWeight: 600,
-//     lineHeight: `76px`,
-//     margin: `8px 0 28px`,
-//     letterSpacing: 0
-// }
 
 export default function Home() {
     return (<div className="index-page" style={{ marginTop: "-64px" }}>
         <div className="banner" style={bannerStyle}>
-            <img src={Brand} alt="Dasdaq" style={{ maxWidth: "24rem" }} >
+            {/* <img src={Brand} alt="Dasdaq" style={{ maxWidth: "24rem" }} >
 
-            </img>
+            </img> */}
 
             <p style={{
                 opacity: 1, transform: `translate(0px, 0px)`, fontSize: `20px`,
@@ -35,7 +27,12 @@ export default function Home() {
             }}>
                 {intl.get('homepage.welcome')}
                 </p>
-            <p> </p>
+                <div className="button-group">
+                    <Button className="btn" type="primary">Btn1</Button>
+                    <Button className="btn" type="primary" size="large">Btn2</Button>
+                    <Button className="btn" type="primary">Btn3</Button>
+                </div>
+                
         </div>
         {/* <img src={logo} alt="给你比心" style={{ maxWidth: "100%" }} /> */}
     </div>)
