5e144fa97fed813c8882c316b178067f951516f7
necokeine <necokeine@gmail.com>
2018-07-23 16:42:16 +0800

    Add button and input bar.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,10 @@
-import React from "react"
+import React from "react";
+import { Button, Input, Select } from "antd";
 import intl from "react-intl-universal";
-// import config from "../config";
-import Brand from "../Brand-Outline.svg";
+import Nasa from "nasa.js";
 
 const backgroundImg = 'https://i.loli.net/2018/07/16/5b4c4a832a920.jpg'
+const Option = Select.Option;
 
 const bannerStyle = {
     padding: `6rem`,
@@ -22,21 +23,80 @@ const bannerStyle = {
 //     letterSpacing: 0
 // }
 
+
+var user_addr;
+var contract = 'n1zofV7tjSZgNqoDMLQNXuLLqdQmM2REAcL';
+
+function BuyEvent(e) {
+    var args = []
+    window.Nasa.call(contract, "buy", args)
+    .then((payId) => {
+        setTimeout(() => {
+        }, 5000)
+    })
+    .catch((e) => {
+        let msg = e.message
+        if (msg === window.Nasa.error.TX_REJECTED_BY_USER) {
+            msg = '您已取消交易！'
+        }
+        alert(msg)
+    })
+}
+
+function SellEvent(e) {
+    alert("Sell");
+}
+
+function ClaimEvent(e) {
+    alert("Claim")
+}
+
+
+export function initializeUserInfo() {
+    window.Nasa.user.getAddr()
+    .then((addr) => {
+        user_addr = addr;
+        //alert(addr)
+    })
+    .catch((e) => {
+        //alert('Error: ' + e)
+    })
+
+    window.Nasa.contract.set({
+        default: {
+            local:   'n1zofV7tjSZgNqoDMLQNXuLLqdQmM2REAcL',
+            testnet: 'n1zofV7tjSZgNqoDMLQNXuLLqdQmM2REAcL',
+            mainnet: 'n1zofV7tjSZgNqoDMLQNXuLLqdQmM2REAcL',
+        },
+        contract_article: {
+            /* ... */
+        },
+        contract_comment: {
+            /* ... */
+        }
+    })
+}
+
 export default function Home() {
-    return (<div className="index-page" style={{ marginTop: "-64px" }}>
+    initializeUserInfo();
+    const selectBefore = (
+        <Select defaultValue="{intl.get('homepage.buy')}" style={{ width: 90 }}>
+            <Option value="{intl.get('homepage.buy')}">{intl.get('homepage.buy')}</Option>
+            <Option value="{intl.get('homepage.sell)}">{intl.get('homepage.sell')}</Option>
+        </Select>
+    )
+    return (
+    <div className="index-page" style={{ marginTop: "-64px" }}>
         <div className="banner" style={bannerStyle}>
-            <img src={Brand} alt="Dasdaq" style={{ maxWidth: "24rem" }} >
-
-            </img>
-
-            <p style={{
-                opacity: 1, transform: `translate(0px, 0px)`, fontSize: `20px`,
-                lineHeight: `40px`,
-            }}>
-                {intl.get('homepage.welcome')}
-                </p>
+            <div> Current balance </div>
+            <div> {intl.get('homepage.buy')}: </div>
+            <Input id="buy_amount" placeholder="{homepage.buy_amount}" />
+            <Input addonBefore={ selectBefore } placeholder="Amount" />
+            <Button type="primary" size="large" onClick={BuyEvent}> {intl.get('homepage.buy_button')}</Button>
+            <Button type="primary" size="large" onClick={SellEvent}> {intl.get('homepage.sell_button')}</Button>
+            <Button type="primary" size="large" onClick={ClaimEvent}> {intl.get('homepage.claim_button')}</Button>
+                
             <p> </p>
         </div>
-        {/* <img src={logo} alt="给你比心" style={{ maxWidth: "100%" }} /> */}
     </div>)
 }
