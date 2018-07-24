98b32dd7d4e701e98ab4f7f5de8c1527cbb8ef85
LookForwardPersistence <fqhua_5@sina.cn>
2018-07-25 01:44:19 +0800

    新增倒计时

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React,{PureComponent} from "react";
 import { Button, Input } from "antd";
 import intl from "react-intl-universal";
 import nasa from "nasa.js";
 import { BigNumber } from 'bignumber.js';
-
+// import styles from './timing.less';
+import moment from 'moment'
 const backgroundImg = 'https://i.loli.net/2018/07/16/5b4c4a832a920.jpg'
 const contract = 'n1vhZgBFYt7AE6nP3VFap9c67VPqn1eFoTi';
 const Nasa = window.Nasa;
@@ -95,6 +96,10 @@ const lableStyle = {
     marginLeft:"0.5rem",
     color:"#000",
     float:"left"
+}
+const  timingStyle = {
+     color: '#fff',
+     fontSize: '3rem'
 }
 // const titleStyle = {
 //     opacity: 1, transform: `translate(0px, 0px)`,
@@ -194,8 +199,12 @@ class SellPopup extends React.Component {
         return (
             <div className='popup' style={popupStyle}>
                 <div className='popup_inner' style={popupInnerStyle}>
-                    <h1>{this.props.text}</h1>
-                    <p> </p>
+                    <h1 style={headerStyle}>{this.props.text}</h1>
+                    <div style={lableStyle}>
+                        Gas:
+                    </div>
+                    <Input id="gas" placeholder="Input a amount in gas" maxLength={25}/>
+                    <div style={lableStyle}>Nas:</div>
                     <Input
                         {...this.props}
                         id="sell_amount"
@@ -214,7 +223,70 @@ class SellPopup extends React.Component {
     }
 }
 
-
+class Timing extends PureComponent {
+    state = {
+        endTime: '2018/07/28 15:39:10',
+        day: '0',
+        hour: '00',
+        minute: '00',
+        second: '00',
+    }
+    componentWillMount() {
+        this.timer = setInterval(
+            () => {
+                let loss = '';
+                const M = moment().format('MM');
+                const D = moment().format('DD');
+                const Y = moment().format('YYYY');
+                const S = moment().format('HH:mm:ss');
+                const now = `${Y}/${M}/${D} ${S}`;
+                const pre = `${Y}/${M}/${D} 23:59:59`;
+                // loss = parseInt((new Date(this.state.endTime) - new Date(now)) / 1000, 10);
+                loss = parseInt((new Date(pre)-new Date(now)) / 1000, 10);
+                if (loss < 0) {
+                    this.setState({
+                        day: '0',
+                        hour: '00',
+                        minute: '00',
+                        second: '00',
+                    });
+                    clearInterval(this.timer);
+                    return;
+                }
+                const day = parseInt((loss * 1) / 86400, 0);
+                loss = (loss * 1) % 86400;
+                let hour = parseInt((loss * 1) / 3600, 0);
+                loss = (loss * 1) % 3600;
+                let minute = parseInt((loss * 1) / 60, 0);
+                let second = (loss * 1) % 60;
+                if (hour * 1 < 10) {
+                    hour = `0${hour}`;
+                }
+                if (minute * 1 < 10) {
+                    minute = `0${minute}`;
+                }
+                if (second * 1 < 10) {
+                    second = `0${second}`;
+                }
+                this.setState({
+                    day,
+                    hour,
+                    minute,
+                    second,
+                });
+            },
+            1000
+        );
+    }
+    render() {
+        const { state } = this;
+        return (
+            <div>
+                <h1 style={timingStyle}>{state.hour}:{state.minute}:{state.second}</h1>
+            </div>
+        );
+    }
+}
 class Home extends React.Component {
 
     componentDidMount() {
@@ -307,6 +379,7 @@ class Home extends React.Component {
                     <div> {intl.get("homepage.contract_balance")}: {this.state.current_balance} NAS</div>
                     <div> {intl.get("homepage.user_addr")}: {this.state.user_addr} </div>
                     <div> {intl.get("homepage.current_price")}: {this.state.current_price} </div>
+                    <Timing/>
                     <Button type="primary" size="large" style={buttonStyle} onClick={this.toggleBuyPopup.bind(this)}>
                         {intl.get('homepage.buy_button')}
                     </Button>
