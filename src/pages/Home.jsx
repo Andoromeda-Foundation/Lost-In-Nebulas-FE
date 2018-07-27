d314cdc53256cb7abe819ef55bfbe89f6548e083
MinakoKojima <minakokojima@MacBook-Pro.local>
2018-07-27 15:19:40 +0800

    go

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -507,7 +507,6 @@ class Home extends React.Component {
                     <div> {intl.get("homepage.user_addr")}: {account} </div>*/}
                     {/*<div style={priceStyle}> {intl.get("homepage.current_price")}: {current_price} NAS</div>*/}
                     <Timing />
-                    <div> {intl.get("homepage.user_addr")}: {account} </div>
                     <Button type="primary" size="large" style={buttonStyle} onClick={this.toggleBuyPopup.bind(this)}>
                         {intl.get('homepage.buy_button')}
                     </Button>
